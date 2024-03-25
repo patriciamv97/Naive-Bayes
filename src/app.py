d23bb8f14dab4ae4c2d36e916f20077364489734
patriciamv97 <72247871+patriciamv97@users.noreply.github.com>
2024-03-25 19:04:35 +0000

    preparacion

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 from utils import db_connect
+import pandas as pd
+
 engine = db_connect()
 
-# your code here
+
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/naive-bayes-project-tutorial/main/playstore_reviews.csv')
+data.to_sql('tabla playstore', engine, if_exists='replace', index=False)
